c28987f18df1ba862fbc176b2b6fa2a0706ffbb4
NHS COVID-19 <68954098+nhs-covid19@users.noreply.github.com>
2020-10-12 21:41:52 +0100

    adding missing configurations, changing package to public

--- android/scenarios/src/main/kotlin/Configurations.kt
+++ android/scenarios/src/main/kotlin/Configurations.kt
@@ -3,6 +3,16 @@ package uk.nhs.covid19.config
 // this is just an example public key generated with:
 // openssl ecparam -genkey -name prime256v1 -noout -out key.txt
 // openssl ec -in key.txt -pubout -out -
+internal val devSignatureKey = SignatureKey(
+    id = "",
+    pemRepresentation = """
+        -----BEGIN PUBLIC KEY-----
+        MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEmRhWDuWAYBdvVQXE3H+ToC2WzKEj
+        bDTBWz6tPlwS3IAaDkLoTu0fwJeRQjzpoxG8C5pYtC1D38ePa0I4NQExqA==
+        -----END PUBLIC KEY-----
+        """.trimIndent()
+)
+
 internal val stagingSignatureKey = SignatureKey(
     id = "",
     pemRepresentation = """
@@ -29,6 +39,23 @@ object Configurations {
             headers = mapOf("Authorization" to ""),
             certificates = amazonRoots,
             signatureKeys = listOf(stagingSignatureKey)
+        )
+    )
+
+    val qa = EnvironmentConfiguration(
+        name = "QA",
+        distributedRemote = Remote(
+            host = "example.com",
+            path = null,
+            certificates = amazonRoots,
+            signatureKeys = listOf(devSignatureKey)
+        ),
+        apiRemote = Remote(
+            host = "example.com",
+            path = null,
+            headers = mapOf("Authorization" to ""),
+            certificates = amazonRoots,
+            signatureKeys = listOf(devSignatureKey)
         )
     )
 
